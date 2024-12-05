801fba65401f83e6807d2aceafc77d8c491bf5a7
Istvan Viczian <viczian.istvan@gmail.com>
2024-12-05 15:20:04 +0100

    Kommunikáció a saga keretein belül

--- career-service/src/main/java/careerservice/enrollment/gateway/CourseGatewayConfiguration.java
+++ career-service/src/main/java/careerservice/enrollment/gateway/CourseGatewayConfiguration.java
@@ -1,5 +1,6 @@
 package careerservice.enrollment.gateway;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.function.Consumer;
 
 @Configuration(proxyBeanMethods = false)
+@AllArgsConstructor
 public class CourseGatewayConfiguration {
 
     private ApplicationEventPublisher applicationEventPublisher;
